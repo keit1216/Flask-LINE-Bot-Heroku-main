71097a6b534d98150c247745463fa98dc70ebf68
ian <keit1216@gmail.com>
2023-06-13 19:33:28 +0800

    imagemap v2

--- app.py
+++ app.py
@@ -38,17 +38,6 @@ def handle_message(event):
     base_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/rich',
     alt_text='this is an imagemap',
     base_size=BaseSize(height=1040, width=1040),
-    video=Video(
-        original_content_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4',
-        preview_image_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg',
-        area=ImagemapArea(
-            x=0, y=0, width=1040, height=585
-        ),
-        external_link=ExternalLink(
-            link_uri='https://line.me',
-            label='See More',
-        ),
-    ),
     actions=[
         URIImagemapAction(
             link_uri='https://google.com/',
