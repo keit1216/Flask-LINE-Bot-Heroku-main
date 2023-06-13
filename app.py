0986f4e40684e03fbbc1e9e13399c6f7dbe40742
ian <keit1216@gmail.com>
2023-06-13 19:29:36 +0800

    imagemap code

--- app.py
+++ app.py
@@ -33,7 +33,38 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, ImagemapSendMessage(json.load(open('imagemap_1.json', 'r', encoding='utf-8'))))
+    # imagemap
+    imagemap_message = ImagemapSendMessage(
+    base_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/rich',
+    alt_text='this is an imagemap',
+    base_size=BaseSize(height=1040, width=1040),
+    video=Video(
+        original_content_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4',
+        preview_image_url='https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg',
+        area=ImagemapArea(
+            x=0, y=0, width=1040, height=585
+        ),
+        external_link=ExternalLink(
+            link_uri='https://line.me',
+            label='See More',
+        ),
+    ),
+    actions=[
+        URIImagemapAction(
+            link_uri='https://google.com/',
+            area=ImagemapArea(
+                x=0, y=586, width=520, height=454
+            )
+        ),
+        MessageImagemapAction(
+            text='hello',
+            area=ImagemapArea(
+                x=520, y=586, width=520, height=454
+            )
+        )
+    ]
+    )
+    line_bot_api.reply_message(event.reply_token, imagemap_message)
     
     msg = event.message.text 
     if(msg == 'temp1'):
