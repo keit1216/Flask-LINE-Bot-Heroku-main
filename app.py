7815810af729bd0acb523eca286615895407f865
ian <keit1216@gmail.com>
2023-06-13 16:20:06 +0800

    edit image url

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
         quick_reply=QuickReply(
             items=[
                 QuickReplyButton(
-                    action=MessageAction(imageUrl="https://cdn-icons-png.flaticon.com/128/3917/3917292.png",label="詢問出團日期",text="詢問出團日期")
+                    action=MessageAction(image_url="https://cdn-icons-png.flaticon.com/128/3917/3917292.png",label="詢問出團日期",text="詢問出團日期")
                     ),
                 QuickReplyButton(
                     action=MessageAction(label="成團資訊",text="成團資訊")
